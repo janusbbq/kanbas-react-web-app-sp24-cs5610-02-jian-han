6c645de1bf07e6fd49d3a84b4e8b4d75a81a5151
somewaykaka <han.jian2@northeastern.edu>
2024-04-24 20:17:00 -0700

    update a6 2.0

--- src/Kanbas/Account/index.tsx
+++ src/Kanbas/Account/index.tsx
@@ -1,23 +1,42 @@
-import { Navigate, Route, Routes } from "react-router";
-import RegisterScreen from "../../Users/register";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import Signin from "../../Users/login";
+import { Navigate, Route, Routes } from "react-router-dom";
+import AccountBreadcrumb from "./AccountBreadcrumb/breadcrumb";
+import AccountNavigation from "./Navigation";
 import Profile from "../../Users/profile";
-import SignIn from "../../Users/login";
+import "./index.css";
+import ProfileEdit from "./Profile/Edit";
 import UserTable from "../../Users/table";
-
-export default function Account() {
-
+import Signup from "../../Users/register";
+function Account() {
   return (
-    <div className="container-fluid wd-content-fill">
-      <h1>Account</h1>
-      <div className="container">
-        <Routes>
-            <Route path="/" element={<Navigate to="login" />} />
+    <div className="wd-main-content">
+      <AccountBreadcrumb />
+      <div className="d-flex">
+        <div className="d-none d-md-block">
+          <AccountNavigation />
+        </div>
+        <div className="wd-internal-content">
+          <Routes>
+            <Route path="/" element={<Navigate to="Signin" />} />
+            <Route path="/Signin" element={<Signin />} />
+            <Route path="/Signup" element={<Signup />} />
+            <Route path="/Profile" element={<Profile />} />
+            <Route path="/Profile/Edit" element={<ProfileEdit />} />
+            <Route path="Notifications" element={<h1>Notifications</h1>} />
+            <Route path="Files" element={<h1>Files</h1>} />
+            <Route path="Settings" element={<h1>Settings</h1>} />
+            <Route path="ePortfolios" element={<h1>ePortfolios</h1>} />
+            <Route path="SharedContent" element={<h1>SharedContent</h1>} />
+            <Route path="TheHub" element={<h1>TheHub</h1>} />
+            <Route path="Qwickly" element={<h1>Qwickly</h1>} />
+            <Route path="Announcements" element={<h1>Announcements</h1>} />
             <Route path="/Admin/Users" element={<UserTable />} />
-            <Route path="/register" element={<RegisterScreen />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/login" element={<SignIn />} />
-        </Routes>
+          </Routes>
+        </div>
       </div>
     </div>
   );
 }
+export default Account;
